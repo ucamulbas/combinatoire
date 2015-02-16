4f6c5b6f2c92ac48b2941d14ecfab33d878371f2
ucamulbas <ucamulbas@hotmail.fr>
2015-02-16 18:39:13 +0100

    ...

--- exo1/main.cpp
+++ exo1/main.cpp
@@ -7,42 +7,21 @@
 using namespace lemon;
 using namespace std;
 
-int main()
+bool biparti()
 {
-  int // tab[4],
-    groupe=0, biparti=0;
+
   ListGraph g;
-  // ListGraph::Node d1 = g.addNode();
-  // ListGraph::Node d2 = g.addNode();
-  // ListGraph::Node g1 = g.addNode();
-  // ListGraph::Node g2 = g.addNode();
-  // ListGraph::Edge e = g.addEdge(d1, g1);
-  // g.addEdge(g1,d2);
-  // g.addEdge(g2,d2);
-  // g.addEdge(g2,d1);
   ListGraph::NodeMap<int> tab(g);
-  //cout << "d1 "<< g.id(d1) << " d2 " << g.id(d2) << " g1 " << g.id(g1) << " g2 " << g.id(g2) << endl;
-  // tab[d1]=2;
-  // tab[d2]=2;
-  // tab[g1]=2;
-  // tab[g2]=2;
-
-  // fstream fexport("mongraphe.lgf", fstream::out);
-  // graphWriter(g, fexport)
-  //   .nodeMap("couleur", tab)
-  //   .run();
-  // fexport.close();
-
+  
   fstream fimport("mongraphe.lgf", fstream::in);
   graphReader(g, fimport)
     .nodeMap("couleur", tab)
     .run();
   fimport.close();
   
+  int groupe = 0;
   for(ListGraph::EdgeIt e(g); e != INVALID; ++e)
     {
-      //cout << "je test les noeuds " << g.id(g.u(e)) << " -> " << g.id(g.v(e)) << endl;
-      // cout << "les id dans tab " << tab[g.id(g.u(e))] << " -> " << tab[g.id(g.v(e))] << endl;
 
       if(tab[g.u(e)]==2)
 	{
@@ -64,10 +43,40 @@ int main()
       if(tab[g.v(e)]==tab[g.u(e)] && tab[g.v(e)] != 2)
 	{
 	  cout << "le graph n'est pas biparti" << endl;
-	  return biparti;
+	  return false;
 	}
     }
-  biparti = 1;
   cout << "le graphe est biparti " << endl;
-  return biparti;
+  return true;
+}
+
+
+int main()
+{
+  bool bip;
+ 
+  // ListGraph::Node d1 = g.addNode();
+  // ListGraph::Node d2 = g.addNode();
+  // ListGraph::Node g1 = g.addNode();
+  // ListGraph::Node g2 = g.addNode();
+  // ListGraph::Edge e = g.addEdge(d1, g1);
+  // g.addEdge(g1,d2);
+  // g.addEdge(g2,d2);
+  // g.addEdge(g2,d1);
+ 
+  //cout << "d1 "<< g.id(d1) << " d2 " << g.id(d2) << " g1 " << g.id(g1) << " g2 " << g.id(g2) << endl;
+  // tab[d1]=2;
+  // tab[d2]=2;
+  // tab[g1]=2;
+  // tab[g2]=2;
+
+  // fstream fexport("mongraphe.lgf", fstream::out);
+  // graphWriter(g, fexport)
+  //   .nodeMap("couleur", tab)
+  //   .run();
+  // fexport.close();
+
+  bip=biparti();
+  cout << bip << endl;
+  return 0;
 }
