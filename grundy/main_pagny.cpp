9fd2b9e47f2dcce5165de41df37874d531497428
ucamulbas <ucamulbas@hotmail.fr>
2015-03-16 18:39:49 +0100

    tri de grundy

--- grundy/main_pagny.cpp
+++ grundy/main_pagny.cpp
@@ -11,14 +11,16 @@ struct comp{
 private:
   ListDigraph &_g;
   ListDigraph::NodeMap<int> &_degre;
+  ListDigraph::NodeMap<int> &_init;
 public:
-  comp(ListDigraph &g, ListDigraph::NodeMap<int> &degre) : _g(g) , _degre(degre) {};
+  comp(ListDigraph &g, ListDigraph::NodeMap<int> &degre, ListDigraph::NodeMap<int> &init ) : _g(g) , _degre(degre) , _init(init)  {};
   
   int operator()(ListDigraph::Node a, ListDigraph::Node b){
-    if(_degre[a] < _degre [b])
-      return 1;
-    else
-      return 0;
+    if(!_init[a] && !_init[b])
+      if(_degre[a] < _degre [b])
+	return 1;
+      else
+	return 0;
   }
 };
 
@@ -26,6 +28,7 @@ int main(void)
 {
   ListDigraph g;
   ListDigraph::NodeMap<int> degre(g);
+  ListDigraph::NodeMap<int> init(g);
   vector<ListDigraph::Node> tab;
   fstream fimport("test.lgf", fstream::in);
   digraphReader(g, fimport)
@@ -34,13 +37,14 @@ int main(void)
   for(ListDigraph::NodeIt nit(g); nit!=INVALID;++nit)
     {
       degre[nit]=0;
+      init[nit]=0;
       for(ListDigraph::OutArcIt ait(g,nit) ; ait!=INVALID;++ait,degre[nit]++);
       cout << g.id(nit) << " est de degre " << degre[nit] << endl;
       tab.push_back(nit);
    }
  
 
-  sort(tab.begin(),tab.end(),comp(g,degre));
+  sort(tab.begin(),tab.end(),comp(g,degre,init));
 
   
   cout <<"__________________________" << endl;
